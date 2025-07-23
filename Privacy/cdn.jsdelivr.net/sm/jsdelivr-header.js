6280168026a3a761b7a21d5ece598edefd0aed46
dufin-hot <blackrat.academy@gmail.com>
2025-07-22 22:15:27 -0300

    Add files via upload

---
+++ Privacy/cdn.jsdelivr.net/sm/jsdelivr-header.js
@@ -0,0 +1,6 @@
+/**
+ * Minified by jsDelivr using Terser v5.15.1.
+ * Original file: /npm/@fortawesome/fontawesome-svg-core@6.4.0/index.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
